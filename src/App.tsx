86666e63eceb83d96f2fd98351b3698aef2e2799
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-08-24 22:01:43 +0300

    е

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header className={s.header}><h1>HEADER</h1></header>
+			<div className={s.body}>
+				<nav className={s.nav} id="sidebar">
+					{/*Для перехода по роутам*/}
+					<NavLink
+						className={({isActive}) => isActive ? s.activeNavLink : s.navLink}
+						to="/sneaker/1">adidas</NavLink>
+					<NavLink
+						className={({isActive}) => isActive ? s.activeNavLink : s.navLink}
+						to="/sneaker/2">puma</NavLink>
+					<NavLink
+						className={({isActive}) => isActive ? s.activeNavLink : s.navLink}
+						to="/sneaker/3">abibas</NavLink>
+				</nav>
+				<div className={s.content}>
+					{/*Группа роутов*/}
+					<Routes>
+						<Route path="/sneaker/1" element={<PageOne/>}/>
+						<Route path="/sneaker/2" element={<PageTwo/>}/>
+						<Route path="/sneaker/3" element={<PageThree/>}/>
+						<Route path="/sneaker/undefined" element={<Error404/>}/>
+						<Route path="/sneaker/*" element={<Navigate to="/sneaker/undefined"/>}/>
+					</Routes>
+				</div>
+			</div>
+			<footer className={s.footer}>abibas 2023</footer>
+		</div>
+	);
 }
+
 
 export default App;
---
+++ src/components/pages/Error404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from '../Site.module.css'
+
+export const Error404 = () => {
+    return (
+        <div className={s.sneaker_error}>
+            Error404
+        </div>
+    );
+};
+
