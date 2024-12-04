486d7a6d786c3ff244fcfa256419847697d70160
yahty01 <yahty01@yandex.ru>
2024-12-05 01:25:11 +0300

    refactor(sidebar): links

--- src/common/components/pages/_styles.ts
+++ src/common/components/pages/_styles.ts
@@ -3,25 +3,40 @@ import styled from "styled-components";
 export const NavWrapper = styled.div`
   margin-left: 10px;
   font-size: 20px;
-  //ве а вложенные в родителя
+  min-height: 25px;
 
   & > a {
+    position: relative;
     text-decoration: none;
     color: #1e3786;
   }
 
   & > a.active {
     text-decoration: none;
-    color: #03eaff;
+    color: #00a3b0;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: -15px;
+      width: 8px;
+      height: 8px;
+      background-color: #7c2323;
+      transform: translate(0, -25%);
+      border-radius: 50%;
+      opacity: 0.7;
+    }
   }
 
   & > a:hover {
-    color: steelblue;
+    color: #7c2323;
   }
 
   & > a.active:hover {
-	  cursor: default;
+    cursor: default;
+    color: #00a3b0;
     text-decoration: none;
-    color: #03eaff;
+
   }
 `
